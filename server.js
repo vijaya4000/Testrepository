436a78740a0ba89824dd664944d6f96a5afdac08
navapetvijaya@gmail.com <navapetvijaya@gmail.com>
2025-05-03 10:35:50 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 console.log("hello world")
+    // Create header
+    const header = document.createElement('div');
+    header.innerText = 'Header';
+    header.style.background = 'lightblue';
+    header.style.padding = '10px';
